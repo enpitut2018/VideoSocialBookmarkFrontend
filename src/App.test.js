efde5eb51052f323c74561eb9a103507b0c8bb18
ishowta <kinironote@gmail.com>
2018-10-12 02:47:00 +0900

    add travis-ci

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import axios from "axios";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('Access backend', () => {
+  return axios
+  .get("http://localhost:3000/")
+  .catch(error => {
+    expect(error.message).not.toEqual('Network Error')
+  });
+})
