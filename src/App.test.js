15275f6eb28d539154c0c310ba972cf08001c7f9
tomoyaf <gazimum@gmail.com>
2018-10-12 03:31:30 +0900

    Fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 import axios from "axios";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it('Access backend', () => {
-  return axios
-  .get("http://localhost:3001/")
-  .catch(error => {
-    expect(error.message).not.toEqual('Network Error')
+it("Access backend", () => {
+  return axios.get("http://localhost:3000/ranking").catch(error => {
+    expect(error.message).not.toEqual("Network Error");
   });
-})
+});
