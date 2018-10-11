6b0e40cb3a089baef87a04a9ebd92e1a168f426f
ishowta <kinironote@gmail.com>
2018-10-12 02:58:18 +0900

    backendを一時的にmockサーバーに変更

--- src/App.test.js
+++ src/App.test.js
@@ -11,7 +11,7 @@ it('renders without crashing', () => {
 
 it('Access backend', () => {
   return axios
-  .get("http://localhost:3000/")
+  .get("http://localhost:3001/")
   .catch(error => {
     expect(error.message).not.toEqual('Network Error')
   });
