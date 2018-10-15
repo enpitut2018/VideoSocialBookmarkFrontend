74949117ce9c26ed5f594ab667b5748a4c21a083
tomoyaf <gazimum@gmail.com>
2018-10-15 23:06:05 +0900

    Add Routing

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Top from "./components/pages/Top";
 import Thread from "./components/pages/Thread";
 import MyPage from "./components/pages/MyPage";
+import Login from "./components/pages/Login";
 import NotFound_404 from "./components/pages/NotFound_404";
 
 class Routes extends React.Component {
@@ -15,6 +16,7 @@ class Routes extends React.Component {
           <Route exact={true} path="/" component={Top} />
           <Route exact={true} path="/threads/:id" component={Thread} />
           <Route exact={true} path="/mypage" component={MyPage} />
+          <Route exact={true} path="/login" component={Login} />
           <Route path="*" component={NotFound_404} />
         </Switch>
       </BrowserRouter>
