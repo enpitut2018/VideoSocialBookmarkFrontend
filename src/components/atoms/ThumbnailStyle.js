87707a03b6b05294a222b7bdb5f20b989de562c6
tomoyaf <gazimum@gmail.com>
2018-10-16 03:12:19 +0900

    Add Thumbnail Placeholder #24

--- src/components/atoms/ThumbnailStyle.js
+++
@@ -1,8 +0,0 @@
-import { css } from "styled-components";
-
-import sizes from "../../theme/sizes";
-
-export default css`
-  width: ${sizes.atoms.Thumbnail.Default.Width};
-  height: ${sizes.atoms.Thumbnail.Default.Height};
-`;
