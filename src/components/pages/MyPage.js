e3a94fd18ad569458f6a6ce23b2651ecfdf6f9b8
tomoyaf <gazimum@gmail.com>
2018-10-15 21:43:30 +0900

    Fix Logo

---
+++ src/components/pages/MyPage.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import MyPageTemplate from "../templates/MyPage";
+
+export default class MyPage extends Component {
+  render() {
+    return <MyPageTemplate />;
+  }
+}
---
+++ src/components/templates/MyPage.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import Wrapper from "../atoms/Wrapper";
+
+export default class MyPageTemplate extends Component {
+  render() {
+    return <Wrapper>hi</Wrapper>;
+  }
+}
