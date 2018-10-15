d50c6b332bb580f6cc4e6aa6f1f962f42498d4cb
tomoyaf <gazimum@gmail.com>
2018-10-16 00:12:51 +0900

    Update Padding of Ranking

--- src/components/organisms/Ranking.js
+++ src/components/organisms/Ranking.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import Title from "../atoms/Title";
+import styled from "styled-components";
 
 import Wrapper from "../atoms/Wrapper";
 import RankingItem from "../molecules/RankingItem";
 import { getRanking } from "../../actions/RankingActions";
+import sizes from "../../theme/sizes.json";
+
+const StyledRanking = styled.div`
+  padding: ${sizes.organisms.Ranking.Default.Padding};
+`;
 
 class Ranking extends Component {
   static fetchData({ dispatch }) {
@@ -19,15 +25,17 @@ class Ranking extends Component {
 
   render() {
     return (
-      <Wrapper dir="column">
-        <Title level={2}>Ranking</Title>
+      <StyledRanking>
         <Wrapper dir="column">
-          {this.props.hasLoaded &&
-            this.props.ranking.map((item, i) => (
-              <RankingItem thread={item} ranking={i + 1} key={item.id} />
-            ))}
+          <Title level={2}>Ranking</Title>
+          <Wrapper dir="column">
+            {this.props.hasLoaded &&
+              this.props.ranking.map((item, i) => (
+                <RankingItem thread={item} ranking={i + 1} key={item.id} />
+              ))}
+          </Wrapper>
         </Wrapper>
-      </Wrapper>
+      </StyledRanking>
     );
   }
 }
