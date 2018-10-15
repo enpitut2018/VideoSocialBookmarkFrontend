73fc494356c4f63e7f9be259f1b531c3a54720a9
tomoyaf <gazimum@gmail.com>
2018-10-15 23:09:16 +0900

    Add Header

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
 
+import Header from "../organisms/Header";
+import Wrapper from "../atoms/Wrapper";
+
 export default class Login extends Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <>
+        <Header />
+        <Wrapper>Login</Wrapper>
+      </>
+    );
   }
 }
