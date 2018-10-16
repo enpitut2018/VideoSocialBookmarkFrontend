d07218e3827797ec00ca1206957971a5e6b1b1bc
NotFounds <m134_warrock_rpg7@yahoo.com>
2018-10-16 17:59:09 +0900

    Fetch user/userIcon data asynchronously

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,4 +1,4 @@
-import { GET_USER } from "../actions/UserActions";
+import { GET_USER_REQUEST, GET_USER_SUCCESS, GET_USER_FAILURE } from "../actions/UserActions";
 
 const initialState = {
   hasLoaded: false,
@@ -7,12 +7,19 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case GET_USER:
+    case GET_USER_REQUEST:
+      return {
+        ...state,
+        hasLoaded: false,
+      };
+    case GET_USER_SUCCESS:
       return {
         ...state,
         hasLoaded: true,
         user: action.user
       };
+    case GET_USER_FAILURE:
+      break;
     default:
       return state;
   }
