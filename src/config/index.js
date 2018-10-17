f7e118237f5ce2230efa52919801dc6846bb72cb
ishowta <kinironote@gmail.com>
2018-10-17 09:36:24 +0900

    fix env

--- src/config/index.js
+++ src/config/index.js
@@ -1,1 +1,1 @@
-module.exports = require(`./${process.env.REACT_APP_ENV !== undefined ? process.env.REACT_APP_ENV : process.env.NODE_ENV}_config`);
+module.exports = require(`./${process.env.REACT_APP_ENV}_config`);
