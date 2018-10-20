2b9874b1c58f9362cb9b539593364ed7169c5e3c
tomoyaf <gazimum@gmail.com>
2018-10-21 07:24:03 +0900

    wip

--- src/components/molecules/EntryItem.js
+++ src/components/molecules/EntryItem.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Wrapper from "../atoms/Wrapper";
 import Text from "../atoms/Text";
 import styled from "styled-components";
+import { style } from "../mediaQuery";
 
 const StyledWrapper = styled(Wrapper)`
   display: flex;
@@ -9,17 +10,30 @@ const StyledWrapper = styled(Wrapper)`
   justify-content: space-between;
   align-items: center;
 
-  width: 800px;
-  margin: 12px 5px;
+  margin: 16px 5px;
+
+  ${style({
+    S: `width: 95%`,
+    M: `width: 90%`,
+    L: `width: 90%`,
+    XL: `width: 800px`
+  })};
 `;
 
 const StyledInnerWrapper = styled(Wrapper)`
+  align-items: flex-start;
   width: 100px;
 `;
 
 const StyledComment = styled(Text)`
   width: 100%;
-  margin-left: 24px;
+
+  ${style({
+    S: `margin-left: 10px`,
+    M: `margin-left: 14px`,
+    L: `margin-left: 18px`,
+    XL: `margin-left: 22px`
+  })};
 `;
 
 export default class EntryItem extends Component {
@@ -27,8 +41,10 @@ export default class EntryItem extends Component {
     return (
       <StyledWrapper>
         <StyledInnerWrapper dir="column">
-          <Text>{this.props.bookmark.user.name || "NoName"}</Text>
-          <Text level="XS">{this.props.bookmark.user.id}</Text>
+          <Text margin="0">{this.props.bookmark.user.name || "NoName"}</Text>
+          <Text margin="0" level="XS">
+            {this.props.bookmark.user.id}
+          </Text>
         </StyledInnerWrapper>
 
         <StyledComment>{this.props.bookmark.comment}</StyledComment>
