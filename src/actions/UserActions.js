4be06934c0a593576e1637c14bd30d9331a48ef9
ishowta <kinironote@gmail.com>
2018-10-26 16:56:35 +0900

    mockを本物に直した

--- src/actions/UserActions.js
+++ src/actions/UserActions.js
@@ -11,33 +11,19 @@ export const GET_USER_BOOKMARKS_REQUEST = "GET_USER_BOOKMARKS_REQUEST";
 export const GET_USER_BOOKMARKS_SUCCESS = "GET_USER_BOOKMARKS_SUCCESS";
 export const GET_USER_BOOKMARKS_FAILURE = "GET_USER_BOOKMARKS_FAILURE";
 
-export const getUserBookmarks = () => dispatch => {
+export const getUserBookmarks = id => dispatch => {
   dispatch({ type: GET_USER_BOOKMARKS_REQUEST });
-  dispatch({
-    type: GET_USER_BOOKMARKS_SUCCESS,
-    bookmarks: [
-      {
-        id: 1,
-        comment: "SYNODOS】方言とコミュニケーション――「ヴァーチャル方言」とその効能／田中ゆかり / 日本語学",
-        entry: {
-          id: 3,
-          url: "https://www.youtube.com/watch?v=5AEbq6X33A8",
-          title: "24/7 lofi hip hop radio - beats to chill/study/relax - YouTube",
-          thumbnail_url: "https://img.youtube.com/vi/5AEbq6X33A8/default.jpg"
-        }
-      },
-      {
-        id: 2,
-        comment: "わいも90分授業を2回ぐらいやったことがあるぜ、スマートフォンとプロセッサ、スマートフォンのOSってので2コマ",
-        entry: {
-          id: 12,
-          url: "https://www.youtube.com/watch?v=ET6657PH9gQ",
-          title: "TANUKI - BABYBABYの夢 - YouTube",
-          thumbnail_url: "https://img.youtube.com/vi/ET6657PH9gQ/default.jpg"
-        }
-      }
-    ]
-  })
+  return axios
+    .get(config.backend_api_url + `/users/${id}`)
+    .then(res => {
+      dispatch({
+        type: GET_USER_BOOKMARKS_SUCCESS,
+        bookmarks: res.data
+      })
+    })
+    .catch(_ => {
+      dispatch({ type: GET_USER_BOOKMARKS_FAILURE , error: "" });
+    });
 }
 
 export const getUser = () => dispatch => {
