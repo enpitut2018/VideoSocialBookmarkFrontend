e20b04a9b2d09150c81390abb7567a0c13174c3d
tomoyaf <gazimum@gmail.com>
2018-10-30 07:02:05 +0900

    update

--- src/components/molecules/Form.js
+++ src/components/molecules/Form.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import Wrapper from "../atoms/Wrapper";
+import styled from "styled-components";
+
+const StyledForm = styled.form`
+  ${props => props.css};
+`;
 
 export default class Form extends Component {
   render() {
     return (
-      <form onSubmit={this.props.onSubmit}>
+      <StyledForm css={this.props.css} onSubmit={this.props.onSubmit}>
         <Wrapper dir="column">{this.props.render()}</Wrapper>
-      </form>
+      </StyledForm>
     );
   }
 }
