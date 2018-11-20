51bee0ea8e4cc2de595da8eed60ec6f202c7a6f7
ishowta <kinironote@gmail.com>
2018-11-21 01:04:07 +0900

    [dev] update ci

--- scripts/update-submodule.sh
+++ scripts/update-submodule.sh
@@ -6,7 +6,7 @@ git config --global user.name "Travis CI"
 
 git submodule update
 cd backend
-git remote add origin-travis https://${GH_TOKEN}@github.com//enpitut2018/VideoSocialBookmark.git
+git remote add origin-travis https://${GH_TOKEN}@github.com/enpitut2018/VideoSocialBookmark.git
 
 git submodule init
 git submodule update
