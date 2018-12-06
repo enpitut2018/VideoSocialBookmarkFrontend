6073faaf4e2738ac6f70f1fd0b336807a17a596d
tomoyaf <gazimum@gmail.com>
2018-12-07 02:42:48 +0900

    表示調整

--- src/components/organisms/PlaylistWrapper.js
+++ src/components/organisms/PlaylistWrapper.js
@@ -6,6 +6,9 @@ import Text from "../atoms/Text";
 import LoadingIcon from "../atoms/LoadingIcon";
 import PlaylistItem from "../molecules/PlaylistItem";
 import styled from "styled-components";
+import colors from "../../theme/colors";
+import palette from "../../theme/palette";
+import elevate from "../../theme/shadows";
 
 const StyledPlaylistWrapper = styled(Wrapper)`
   display: flex;
@@ -19,15 +22,38 @@ class PlaylistWrapper extends React.Component {
   }
 
   render() {
+    const currentIndex =
+      this.props.playlist !== undefined &&
+      this.props.playlist !== null &&
+      this.props.playlist.playlist_items.findIndex(
+        item => item.entry.id === this.props.entryId
+      );
     return (
       <>
         {this.props.playlist === undefined ? (
           <LoadingIcon />
         ) : (
           this.props.playlist !== null && (
-            <Wrapper dir="column" css="margin-left: 2rem;">
-              <Text level="L" margin="10px 0 13px 0">
+            <Wrapper
+              dir="column"
+              css={`
+                margin-left: 2rem;
+                padding: 0.2rem 1.2rem 0.2rem 1rem;
+                background-color: ${palette[
+                colors.organisms.PlaylistWrapper.Background
+              ]};
+                ${elevate(2)};
+              `}
+            >
+              <Text size="M" margin="10px 0 3px 0">
+                プレイリスト
+              </Text>
+              <Text size="L" margin="3px 0 3px 0">
                 {this.props.playlist.name}
+              </Text>
+              <Text size="M" margin="0 0 18px 0">
+                {currentIndex + 1 + " "}/
+                {" " + this.props.playlist.playlist_items.length}
               </Text>
               <StyledPlaylistWrapper>
                 {this.props.playlist.playlist_items.map((item, index) => (
